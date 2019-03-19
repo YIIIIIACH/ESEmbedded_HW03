446900fb334e62f5312e2299dc805827fa656fee
YIIIIIACH <1997marc212@gmail.com>
2019-03-19 21:00:06 +0800

    finish in 03/18/2019 20:59

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
 void reset_handler(void)
 {
-	while (1)
-		;
+	int a =1;
+	a++;
+	a--;
+	int b =2;
+	int c =3;
+	int d =4;
+	int result;
+	result= compute(a,b,c,d);
 }
+
+int compute(int a,int b,int c,int d )
+{
+	return a+b*sub(d,c);
+}
+
+int sub(int d,int c)
+{
+	return d-c;
+}
+
